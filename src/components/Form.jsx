1e5dda2d72a0c3c2eec0f35f27bec4cbd6ada171
mu <mulinyan@amazon.com>
2017-10-29 16:35:04 -0700

    lesson 5

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 
+import FormErrors from './FormErrors'
+
 class Form extends Component {
   constructor (props) {
     super(props)
@@ -10,17 +12,81 @@ class Form extends Component {
         username: '',
         email: '',
         password: ''
-      }
+      },
+      formRules: [
+        {
+          id: 1,
+          field: 'username',
+          name: 'Username must be greater than 5 characters.',
+          valid: false
+        },
+        {
+          id: 2,
+          field: 'email',
+          name: 'Email must be greater than 5 characters.',
+          valid: false
+        },
+        {
+          id: 3,
+          field: 'email',
+          name: 'Email must be a valid email address.',
+          valid: false
+        },
+        {
+          id: 4,
+          field: 'password',
+          name: 'Password must be greater than 10 characters.',
+          valid: false
+        }
+      ],
+      valid: false
     }
     this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
   }
   componentDidMount() {
     this.clearForm();
   }
+  initRules() {
+    const rules = this.state.formRules;
+    for (const rule of rules) {
+      rule.valid = false;
+    }
+    this.setState({formRules: rules})
+  }
   componentWillReceiveProps(nextProps) {
     if (this.props.formType !== nextProps.formType) {
       this.clearForm();
+      this.initRules();
     }
+  }
+  validateForm() {
+    const formType = this.props.formType;
+    const rules = this.state.formRules;
+    const formData = this.state.formData;
+    this.setState({valid: false});
+    for (const rule of rules) {
+      rule.valid = false;
+    }
+    if (formType === 'register') {
+      if (formData.username.length > 5) rules[0].valid = true;
+    }
+    if (formType === 'login') rules[0].valid = true;
+    if (formData.email.length > 5) rules[1].valid = true;
+    if (this.validateEmail(formData.email)) rules[2].valid = true;
+    if (formData.password.length > 10) rules[3].valid = true;
+    this.setState({formRules: rules})
+    if (this.allTrue()) this.setState({valid: true});
+  }
+  validateEmail(email) {
+    // eslint-disable-next-line
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  allTrue() {
+    for (const rule of this.state.formRules) {
+      if (!rule.valid) return false;
+    }
+    return true;
   }
   clearForm() {
     this.setState({
@@ -31,6 +97,7 @@ class Form extends Component {
     const obj = this.state.formData;
     obj[event.target.name] = event.target.value;
     this.setState(obj);
+    this.validateForm();
   }
   handleUserFormSubmit(event) {
     event.preventDefault();
@@ -65,6 +132,10 @@ class Form extends Component {
       <div>
         <h1 style={{'textTransform':'capitalize'}}>{this.props.formType}</h1>
         <hr/><br/>
+        <FormErrors
+          formType={this.props.formType}
+          formRules={this.state.formRules}
+        />
         <form onSubmit={(event) => this.handleUserFormSubmit(event)}>
           {this.props.formType === 'register' &&
             <div className="form-group">
@@ -105,6 +176,7 @@ class Form extends Component {
             type="submit"
             className="btn btn-primary btn-lg btn-block"
             value="Submit"
+            disabled={!this.state.valid}
           />
         </form>
       </div>
